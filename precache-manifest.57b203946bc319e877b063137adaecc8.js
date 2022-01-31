c24e897ba007c34e0a8cce53eade3eb33693771c
Shumakher <gzullm02041963@googlemail.com>
2022-01-31 13:03:05 +0300

    first production m4

---
+++ precache-manifest.57b203946bc319e877b063137adaecc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60b79098cbc76584c080a8612cf285d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7319640e294a08a3277",
+    "url": "/static/css/main.33e15a71.chunk.css"
+  },
+  {
+    "revision": "c9f5fe7d30ac1e54250e",
+    "url": "/static/js/2.f7ee8a42.chunk.js"
+  },
+  {
+    "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
+    "url": "/static/js/2.f7ee8a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7319640e294a08a3277",
+    "url": "/static/js/main.ea9b3a94.chunk.js"
+  },
+  {
+    "revision": "97d614f48baeada74120",
+    "url": "/static/js/runtime-main.47eaace9.js"
+  }
+]);
