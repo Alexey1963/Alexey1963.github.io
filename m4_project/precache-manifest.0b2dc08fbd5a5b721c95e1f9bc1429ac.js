7d446c8bcf1a5a7b2bbd319e35108eb8bc7d2bb0
Shumakher <gzullm02041963@googlemail.com>
2022-01-31 13:59:45 +0300

    'www'

--- m4_project/precache-manifest.0b2dc08fbd5a5b721c95e1f9bc1429ac.js
+++ m4_project/precache-manifest.0b2dc08fbd5a5b721c95e1f9bc1429ac.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "82df769f627b7419c88e631d2db7b2d9",
-    "url": "/index.html"
+    "url": "/m4_project/index.html"
   },
   {
     "revision": "84c5429a622a81390cca",
-    "url": "/static/css/main.33e15a71.chunk.css"
+    "url": "/m4_project/static/css/main.33e15a71.chunk.css"
   },
   {
     "revision": "c9f5fe7d30ac1e54250e",
-    "url": "/static/js/2.f7ee8a42.chunk.js"
+    "url": "/m4_project/static/js/2.f7ee8a42.chunk.js"
   },
   {
     "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
-    "url": "/static/js/2.f7ee8a42.chunk.js.LICENSE.txt"
+    "url": "/m4_project/static/js/2.f7ee8a42.chunk.js.LICENSE.txt"
   },
   {
     "revision": "84c5429a622a81390cca",
-    "url": "/static/js/main.43fb640d.chunk.js"
+    "url": "/m4_project/static/js/main.43fb640d.chunk.js"
   },
   {
     "revision": "97d614f48baeada74120",
-    "url": "/static/js/runtime-main.47eaace9.js"
+    "url": "/m4_project/static/js/runtime-main.47eaace9.js"
   }
 ]);
--- m4_project/precache-manifest.44d20915f176fe24dce6b97521417eec.js
+++ m4_project/precache-manifest.44d20915f176fe24dce6b97521417eec.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "bfc7a14330076951be4dfe427ac29067",
-    "url": "/index.html"
+    "url": "/m4_project/index.html"
   },
   {
     "revision": "39981fc15dffbc39a5b9",
-    "url": "/static/css/main.33e15a71.chunk.css"
+    "url": "/m4_project/static/css/main.33e15a71.chunk.css"
   },
   {
     "revision": "c9f5fe7d30ac1e54250e",
-    "url": "/static/js/2.f7ee8a42.chunk.js"
+    "url": "/m4_project/static/js/2.f7ee8a42.chunk.js"
   },
   {
     "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
-    "url": "/static/js/2.f7ee8a42.chunk.js.LICENSE.txt"
+    "url": "/m4_project/static/js/2.f7ee8a42.chunk.js.LICENSE.txt"
   },
   {
     "revision": "39981fc15dffbc39a5b9",
-    "url": "/static/js/main.707d0b8a.chunk.js"
+    "url": "/m4_project/static/js/main.707d0b8a.chunk.js"
   },
   {
     "revision": "97d614f48baeada74120",
-    "url": "/static/js/runtime-main.47eaace9.js"
+    "url": "/m4_project/static/js/runtime-main.47eaace9.js"
   }
 ]);
